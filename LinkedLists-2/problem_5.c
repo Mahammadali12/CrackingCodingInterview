d4e5dd0c6f6395fb848853cf3be0a1c4cdd8935f
Mahammadali12 <zamanli.mehemmedeli@gmail.com>
2025-01-12 15:30:02 -0500

    Solved problem 5

--- LinkedLists-2/problem_5.c
+++ LinkedLists-2/problem_5.c
@@ -2,27 +2,65 @@
 #include <malloc.h>
 #include "list.h"
 
-struct node* head;
+struct node* head_1;
+struct node* head_2;
 
 void reverseList(struct node* head);
 
 
 int main (void)
 {
-    head = (struct node*) malloc(sizeof(struct node));
-
-    for (int i = 1; i < 3; i++)
+    head_1 = (struct node*) malloc(sizeof(struct node));
+    head_1->value = 1;
+    
+    for (int i = 2; i < 4; i++)
     {
-        add_to_end(i,head);
+        add_to_end(i,head_1);
+    }
+    // printList(head_1);
+
+
+    head_2 = (struct node*) malloc(sizeof(struct node));
+    head_2->value = 3;
+
+    for (int i = 5; i < 7; i++)
+    {
+        add_to_end(i,head_2);
     }
 
-    printList(head);
 
-    reverseList(head);
+    int number_1;
+    int buf=1;
+    struct node* temp_1 = head_1;
+    while (temp_1 != NULL)
+    {
+        number_1 = number_1 + temp_1->value*buf;
+        buf *=10;
+        temp_1 = temp_1->next;
+    }
 
-    printList(head);
 
-    free_list(head);   
+    int number_2;
+    buf=1;
+    struct node* temp = head_2;
+    while (temp != NULL)
+    {
+        number_2 = number_2 + temp->value*buf;
+        buf *=10;
+        temp = temp->next;
+    }
+
+
+    printf("%i\n",number_1);
+    printf("%i\n",number_2);
+    
+    printf("%i + %i\n",number_1,number_2);
+    
+    printList(head_1);
+    printList(head_2);
+
+    free_list(head_1);   
+    free_list(head_2);   
 }
 
 
@@ -44,6 +82,6 @@ void reverseList(struct node* Head)
     }
     temp_3->next = temp_2;
     temp_2->next = temp_1;
-    head = temp_3;
+    // head = temp_3;
     
 }
